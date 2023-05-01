c3c30f4da78063044d9f23569c2072a7729de622
ipallares <ipt@condat.de>
2023-05-01 18:34:44 +0200

    Use Todo Entity/Repo and show in frontend

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\TodoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController extends AbstractController
 {
-    #[Route('/todo', name: 'app_todo')]
-    public function index(): Response
+    #[Route('/todo', name: 'app_todo', methods: ['GET'])]
+    public function index(TodoRepository $todoRepository): Response
     {
+        $todoList = $todoRepository->findAll();
+
         return $this->render('todo/index.html.twig', [
             'controller_name' => 'TodoController',
+            'todoList' => $todoList
         ]);
     }
 }
