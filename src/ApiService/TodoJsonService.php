eaf52be884d2d9969246f080fc5bac08fce0f285
ipallares <ipt@condat.de>
2023-05-03 18:30:20 +0200

    work with env configurations

--- src/ApiService/TodoJsonService.php
+++ src/ApiService/TodoJsonService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\ApiService;
 
 use App\Interface\Service\TodoServiceInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 
@@ -12,13 +13,21 @@ class TodoJsonService
 {
     public function __construct(
         private readonly TodoServiceInterface $todoService,
-        private readonly SerializerInterface $serializer
+        private readonly SerializerInterface $serializer,
+        private readonly LoggerInterface $logger,
+        private readonly string $apiUrl,
+        private readonly string $apiUser,
+        private readonly string $apiSecret,
     ) {
     }
 
     public function findAllAsJson(): string
     {
         $todoList = $this->todoService->findAll();
+
+        $this->logger->info('ApiUrl: {apiUrl}', ['apiUrl' => $this->apiUrl]);
+        $this->logger->info('ApiUser: {apiUser}', ['apiUrl' => $this->apiUser]);
+        $this->logger->info('ApiUrl: {apiSecret}', ['apiSecret' => $this->apiSecret]);
 
         return $this->serializer->serialize($todoList, 'json', ['json_encode_options' => JsonResponse::DEFAULT_ENCODING_OPTIONS,]);
     }
